d7950e1e718eb1ff26da358b4610ecbbe4925e72
rbbrown <rbbrown@bju.edu>
2024-08-09 11:56:34 -0400

    Save system - part 1

--- global.gd
+++ global.gd
@@ -2,13 +2,19 @@ extends Node
 
 const SAVE_DIR = "user://saves/"
 const SAVE_FILE_NAME = "save.json"
-const SECURITY_KEY = "082932H33"
+const SECURITY_KEY = "089SADFH"
 
+var player_data = PlayerData.new()
 
-@onready var coins: int = 0
-@onready var gems: int = 0
+func _ready():
+	verify_save_directory(SAVE_DIR)
 
-# Array = price, purchased
-var cosmetics = {
-	"hat": [350,false]
-}
+func verify_save_directory(path: String):
+	DirAccess.make_dir_absolute(path)
+
+func save_data(path: String):
+	var file = FileAccess.open_encrypted_with_pass(path, FileAccess.WRITE,SECURITY_KEY)
+	if file == null:
+		print(FileAccess.get_open_error())
+		return
+
