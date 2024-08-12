e47016f5ea21d7c47d5003646f362751768c3742
rbbrown <rbbrown@bju.edu>
2024-08-12 11:45:58 -0400

    Added PlayerData for auto default stuff

--- PlayerData.gd
+++ PlayerData.gd
@@ -1,2 +1,77 @@
 extends Resource
 class_name PlayerData
+
+@export var coins: int = 200
+@export var gems: int = 5
+@export var shop: Dictionary = {
+			"cos_1": {
+				"name": "Cow Beanie",
+				"image": "cow_hat",
+				"cost": 300,
+				"purchased": false,
+				"description": "Moo-ve over ordinary hats, this beanie brings all the charm of a farmyard friend!"
+			},
+			"cos_2": {
+				"name": "Pig Beanie",
+				"image": "pig_hat",
+				"cost": 600,
+				"purchased": false,
+				"description": "Snout-tastic and snug! This pig beanie is sure to make you the cutest critter around."
+			},
+			"cos_3": {
+				"name": "Unicorn Headband",
+				"image": "unicorn_headband",
+				"cost": 800,
+				"purchased": false,
+				"description": "Sparkle and shine with this magical unicorn headband. Perfect for turning any day into a fairy tale!"
+			},
+			"cos_4": {
+				"name": "Rainbow Scarf",
+				"image": "rainbow_scarf",
+				"cost": 500,
+				"purchased": false,
+				"description": "Brighten up your outfit with this vibrant rainbow scarf. It’s like wearing a rainbow hug!"
+			},
+			"cos_5": {
+				"name": "Cat Ears Headband",
+				"image": "cat_ears_headband",
+				"cost": 400,
+				"purchased": false,
+				"description": "Purrfect for feline fans! These cat ears are an adorable way to add a touch of whimsy to your look."
+			},
+			"cos_6": {
+				"name": "Starry Night Hat",
+				"image": "starry_night_hat",
+				"cost": 700,
+				"purchased": false,
+				"description": "Gaze at the stars and dream big with this enchanting hat covered in a galaxy of glittering stars."
+			},
+			"cos_7": {
+				"name": "Bunny Ears Headband",
+				"image": "bunny_ears_headband",
+				"cost": 350,
+				"purchased": false,
+				"description": "Hop into cuteness with this adorable bunny ears headband. It's a fluffy way to add some fun to your outfit!"
+			},
+			"cos_8": {
+				"name": "Dragon Scale Cape",
+				"image": "dragon_scale_cape",
+				"cost": 900,
+				"purchased": false,
+				"description": "Unleash your inner dragon with this stunning cape. Each scale sparkles with a hint of magic!"
+			},
+			"cos_9": {
+				"name": "Cherry Blossom Kimono",
+				"image": "cherry_blossom_kimono",
+				"cost": 650,
+				"purchased": false,
+				"description": "Wrap yourself in the elegance of spring with this cherry blossom kimono. It’s like wearing a garden of flowers!"
+			},
+			"cos_10": {
+				"name": "Robot Helmet",
+				"image": "robot_helmet",
+				"cost": 550,
+				"purchased": false,
+				"description": "Gear up for a futuristic adventure with this sleek robot helmet. Perfect for tech enthusiasts and adventurers alike!"
+			}
+		}
