cb082e66b624f0b8f027da2bc6b5835b46e62e42
ibalarde <isaac.balardelle@gmail.com>
2024-08-16 19:51:47 -0400

    tweaked shop buttons and added buy_item

--- Scripts/Shop.gd
+++ Scripts/Shop.gd
@@ -1,13 +1,33 @@
 extends Control
 
+var item_button = preload("res://Menus/ItemButton.tscn")
+# Adds item to player inventory and subtracts the gold.
+func buy_item(item_id):
+	# Check if item is valid
+	if item_id not in Global.player_data.shop:
+		printerr("item does not exist")
+		return
+	var item = Global.player_data.shop[item_id]
+	if item["purchased"]:
+		return
+	if item["cost"] > Global.player_data.coins:
+		print("not enough money")
+		return
+	else:
+		Global.player_data.shop[item_id]["purchased"] = true
+		Global.player_data.gold -= item["cost"]
+	
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for shop_item in Global.player_data.shop.keys():
-		var item_btn = Label.new()
-		item_btn.text = "hello world"
+		var item_name = Global.player_data.shop[shop_item]["name"]
+		#var item_btn = Label.new()
+		#item_btn.text = item_name
+		var item_btn = item_button.instantiate()
+		print(item_btn.position)
 		$ColorRect/VBoxMain/ItemScrollbar/ItemsContainer.add_child(item_btn)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -16,3 +36,10 @@ func _process(delta):
 
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://Menus/main_menu.tscn")
+
+
+func _on_panel_container_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			print("item clicked")
+
