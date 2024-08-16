32222a61c8e92f0681ac9823c7b9329bebcd69e2
Isaac Balardelle <isaac.balardelle@gmail.com>
2024-08-16 15:41:17 -0400

    working on shop

--- Scripts/Shop.gd
+++ Scripts/Shop.gd
@@ -3,11 +3,15 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	for shop_item in Global.player_data.shop.keys():
+		print(Global.player_data.shop[shop_item]["name"])
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	$ColorRect/VBoxMain/TopStats/CoinLabel.text = "Coins: " + str(Global.player_data.coins)
+	
 	pass
 
 func _on_back_button_pressed():
